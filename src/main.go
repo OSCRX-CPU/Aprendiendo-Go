ea879c584a3e60742a38640a9d09a4b325ea0d5d
OSCRX-CPU <oscarcanales10000@gmail.com>
2024-07-06 00:20:29 -0600

    tercer commit

--- src/main.go
+++ src/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Oscar")
+	// Declaracion de Constantes
+	const pi float64 = 3.14
+	const pi2 = 3.14
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2", pi2)
+
+	// Declaracion de Variables Enteras
+	Base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(Base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+	// Area cuadrado
+	const basecuadrado = 10
+	areacuadrado := basecuadrado * basecuadrado
+	fmt.Println("Area cuadrado:", areacuadrado)
 }
